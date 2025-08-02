3e32a7085846227646fc76dc98496909f8023d43
Mohammad <mhmd7776@yahoo.com>
2025-08-02 21:22:32 +0330

    Add discount service Add grpc proto and service file

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,29 @@
+﻿using Discount.Grpc.Protos;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            return base.GetDiscount(request, context);
+        }
+
+        public override Task<Int32Value> CreateDiscount(CouponModel request, ServerCallContext context)
+        {
+            return base.CreateDiscount(request, context);
+        }
+
+        public override Task<BoolValue> UpdateDiscount(CouponModel request, ServerCallContext context)
+        {
+            return base.UpdateDiscount(request, context);
+        }
+
+        public override Task<Empty> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }
+    }
+}
